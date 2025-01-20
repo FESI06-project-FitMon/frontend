435949c2389dff1041b49c8b011b2a7279880c6c
moonjw0 <jjwon1230@naver.com>
2025-01-20 17:25:26 +0900

    💄 design: 회원가입 반응형 적용

--- src/pages/signup/components/SignupForm.tsx
+++ src/pages/signup/components/SignupForm.tsx
@@ -111,7 +111,7 @@ export default function SignupForm() {
   return (
     <form
       onSubmit={handleSignupSubmit}
-      className="flex flex-col w-full px-6 gap-6"
+      className="flex flex-col w-full px-6 gap-8 md:gap-6"
     >
       <FormField
         label="닉네임"
@@ -158,7 +158,7 @@ export default function SignupForm() {
         errorMessage="비밀번호가 일치하지 않습니다."
       />
       <Button type="submit" name="회원가입" className="h-16 mt-3" />
-      <div className="flex flex-row justify-end mt-3">
+      <div className="flex flex-row justify-center md:justify-end -mt-2 md:mt-3">
         <p className="mr-4 text-[1rem]">{'이미 회원이신가요?'}</p>
         <p
           onClick={() => router.push('/login')}
