57b48510548fe1f9df1d411d2d2487994414e4ad
songyeonji <dus3439@naver.com>
2025-01-22 18:31:57 +0900

    🐛 fix: 쓸데없는get은 왜 들어가 있었지,,

--- src/pages/mypage/api/profileService.ts
+++ src/pages/mypage/api/profileService.ts
@@ -8,13 +8,6 @@ interface ProfileUpdate {
 }
 
 export const profileService = {
-  // 프로필 정보 조회
-  getProfile: async () => {
-    return await apiRequest<UserProfile>({
-      param: 'api/v1/my-page/profile',
-      method: 'get'
-    });
-  },
 
   updateProfile: async (data: ProfileUpdate) => {
     return await apiRequest<ProfileUpdate>({
