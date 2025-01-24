5f93c76ff361d653cbf2d3aa0d4f5e5994ad0110
sumin305 <gogotnals@naver.com>
2025-01-24 11:39:30 +0900

    🐛 fix: GatheringType 변경

--- src/pages/mypage/components/join-gathering/JoinGathering.tsx
+++ src/pages/mypage/components/join-gathering/JoinGathering.tsx
@@ -1,18 +1,22 @@
-import { useParticipatingGatherings, useCancelParticipation, useGatheringChallenges } from '@/pages/mypage/service/myGathering';
+import {
+  useParticipatingGatherings,
+  useCancelParticipation,
+  useGatheringChallenges,
+} from '@/pages/mypage/service/myGathering';
 import GatheringList from '@/pages/mypage/components/gathering-section/GatheringList';
 import { StateData } from '@/components/common/StateData';
 
 export default function JoinGathering() {
   const { data: gatheringsData, isLoading } = useParticipatingGatherings();
   const { mutateAsync: cancelParticipation } = useCancelParticipation();
-  const { data: gatheringChallenges = {} } = useGatheringChallenges(gatheringsData, false);
+  const { data: gatheringChallenges = {} } = useGatheringChallenges(
+    gatheringsData,
+    false,
+  );
 
   if (isLoading || !gatheringsData?.content?.length) {
     return (
-      <StateData
-        isLoading={isLoading}
-        emptyMessage="참여한 모임이 없습니다."
-      />
+      <StateData isLoading={isLoading} emptyMessage="참여한 모임이 없습니다." />
     );
   }
 
