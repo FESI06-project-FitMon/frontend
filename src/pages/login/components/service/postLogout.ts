1aa48d8a3a39964b58b5c5f458e333e8992ed5d3
moonjw0 <jjwon1230@naver.com>
2025-01-27 15:26:04 +0900

    ♻️ refactor: 로그아웃 요청함수 에러 타입 수정

--- src/pages/login/components/service/postLogout.ts
+++ src/pages/login/components/service/postLogout.ts
@@ -2,19 +2,23 @@ import apiRequest from '@/utils/apiRequest';
 import { useMutation } from '@tanstack/react-query';
 import useMemberStore from '@/stores/useMemberStore';
 import router from 'next/router';
+import { AxiosError } from 'axios';
+
+// 로그아웃 요청 함수
 async function postLogout(): Promise<void> {
-  await apiRequest({ param: 'api/v1/logout', method: 'post' });
+  return await apiRequest({ param: 'api/v1/logout', method: 'post' });
 }
+
 // 로그아웃 요청 Mutation 함수
 export const useLogoutMutation = () =>
-  useMutation<void, Error>({
+  useMutation<void, AxiosError>({
     mutationFn: postLogout,
     onSuccess: () => {
       localStorage.removeItem('isLogin');
       useMemberStore.getState().setIsLogin(false);
       router.push('/');
     },
-    onError: (error: Error) => {
+    onError: (error: AxiosError) => {
       console.log(error);
     },
   });
