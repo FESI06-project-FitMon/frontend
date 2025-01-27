3681f49e2110d6c2b6ac9f89b6f14b6f6b1ca26b
moonjw0 <jjwon1230@naver.com>
2025-01-27 11:49:16 +0900

    ✨ feat: 유효성검사 조건 변경

--- src/utils/formValidation.ts
+++ src/utils/formValidation.ts
@@ -18,30 +18,29 @@ export default function formValidation({
   if (value === '') {
     return false;
   }
+
   // 닉네임: 2자 이상 10자 이하
   if (name === 'nickName') {
     return value.length < 2 || value.length > 10;
   }
+
   // 이메일: 이메일 형식
   else if (name === 'email') {
     return value === '' || !emailRegex.test(value);
   }
+
   // (회원가입) 비밀번호: 8자 이상
   else if (name === 'password' && formType === 'signup') {
     return value.length < 8;
   }
-  // (로그인) 비밀번호: 빈 칸 불가능
-  // 첫 번째는 막는 방법이 없음
-  // else if (name === 'password' && formType === 'login') {
-  //   return value === '';
-  // }
 
   // 비밀번호 확인: 비밀번호와 동일
   else if (name === 'passwordCheck') {
     return value === '' || value !== password;
   }
+
   // 나머지
   else {
-    return true;
+    return false;
   }
 }
