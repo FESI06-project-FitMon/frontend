16fd12adcee6ebddcab0e04f82600c6840c37178
sumin305 <gogotnals@naver.com>
2025-01-28 22:32:07 +0900

    ✨ feat: 모임 API 추가

--- src/pages/detail/api/gatheringApi.ts
+++ src/pages/detail/api/gatheringApi.ts
@@ -27,23 +27,46 @@ export const fetchGatheringStatus = async (gatheringId: number) => {
   }
 };
 
-// 방명록 불러오기 API
-export const fetchGatheringGuestbooks = async (
-  gatheringId: number,
-  page: number = 0,
-  pageSize: number = 4,
-): Promise<GatheringGuestbookResponse> => {
+// 모임 참가하기
+export const participantGathering = async (gatheringId: number) => {
   try {
-    return await apiRequest<GatheringGuestbookResponse>({
-      param: `/api/v1/gatherings/${gatheringId}/guestbooks?page=${page}&pageSize=${pageSize}`,
-      method: 'get',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return await apiRequest<any>({
+      param: `/api/v1/gatherings/${gatheringId}/participants`,
+      method: 'post',
     });
   } catch (error) {
     throw error;
   }
 };
 
-// 모임 수정하기 API
+// 모임 참여 취소하기
+export const cancelGathering = async (gatheringId: number) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return await apiRequest<any>({
+      param: `/api/v1/gatherings/${gatheringId}/cancel`,
+      method: 'delete',
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 모임 취소하기 (모임장)
+export const deleteGathering = async (gatheringId: number) => {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return await apiRequest<any>({
+      param: `/api/v1/gatherings/${gatheringId}`,
+      method: 'delete',
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 모임 수정하기 API (모임장)
 export const updateGathering = async (
   gatheringUpdateRequest: GatheringUpdateRequest,
   gatheringId: number,
@@ -54,6 +77,22 @@ export const updateGathering = async (
       param: '/api/v1/gatherings/' + gatheringId,
       method: 'patch',
       requestData: gatheringUpdateRequest,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 방명록 불러오기 API
+export const fetchGatheringGuestbooks = async (
+  gatheringId: number,
+  page: number = 0,
+  pageSize: number = 4,
+): Promise<GatheringGuestbookResponse> => {
+  try {
+    return await apiRequest<GatheringGuestbookResponse>({
+      param: `/api/v1/gatherings/${gatheringId}/guestbooks?page=${page}&pageSize=${pageSize}`,
+      method: 'get',
     });
   } catch (error) {
     throw error;
