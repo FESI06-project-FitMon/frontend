03269cac999aebae29b07cbbce57d88a20b0908d
sumin305 <gogotnals@naver.com>
2025-02-05 20:48:57 +0900

    ✨ feat: 반응형 구현

--- src/pages/detail/components/GatheringGuestbookCard.tsx
+++ src/pages/detail/components/GatheringGuestbookCard.tsx
@@ -2,7 +2,8 @@ import Heart from '@/components/common/Heart';
 import Image from 'next/image';
 
 interface GuestbookProps {
-  guestbook: {  // 중첩된 guestbook 객체를 제거
+  guestbook: {
+    // 중첩된 guestbook 객체를 제거
     guestbookId: number;
     content: string;
     rating: number;
@@ -15,14 +16,13 @@ interface GuestbookProps {
   };
 }
 
-
-export default function Guestbook({ guestbook }: GuestbookProps) {  
+export default function Guestbook({ guestbook }: GuestbookProps) {
   return (
     <div className="flex flex-col w-full  bg-dark-200 rounded-[10px] gap-5 p-[30px]">
       <div className="flex justify-between">
         <Heart rating={guestbook.rating} />
         <p className="flex md:hidden text-[13px] md:text-sm text-dark-700 content-center">
-          {`${guestbook.createDate.substring(0, 10)}`}
+          {`${guestbook.createdAt.substring(0, 10)}`}
         </p>
       </div>
       <p className="w-full h-min-[72px] text-sm md:text-base text-ellipsis overflow-hidden break-words">
@@ -41,10 +41,9 @@ export default function Guestbook({ guestbook }: GuestbookProps) {
         </div>
         {/* 날짜 */}
         <p className="hidden md:flex text-[13px] md:text-sm text-dark-700 content-center">
-          {`${guestbook.createDate.substring(0, 10)}`}
+          {`${guestbook.createdAt.substring(0, 10)}`}
         </p>
       </div>
     </div>
   );
 }
-
