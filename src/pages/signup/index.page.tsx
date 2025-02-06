070e0f89b4bd2bc29420871d02683aa3f7b32589
moonjw0 <jjwon1230@naver.com>
2025-02-06 16:40:53 +0900

    ✨ feat: 회원가입 페이지 메타데이터 추가

--- src/pages/signup/index.page.tsx
+++ src/pages/signup/index.page.tsx
@@ -1,14 +1,21 @@
+import { Metadata } from '@/components/common/Metadata';
 import SignupForm from './components/SignupForm';
 
 export default function Signup() {
   return (
-    <div className="w-full h-[calc(100vh-81px)] flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center w-full max-w-[640px] md:min-w-[640px]">
-        <h1 className="mb-7 md:mb-12 text-[1.5rem] md:text-[2.25rem] font-medium">
-          {'회원가입'}
-        </h1>
-        <SignupForm />
+    <>
+      <Metadata
+        title="회원가입"
+        description="회원가입을 통해 FitMon의 회원이 되어보세요!"
+      />
+      <div className="w-full h-[calc(100vh-81px)] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full max-w-[640px] md:min-w-[640px]">
+          <h1 className="mb-7 md:mb-12 text-[1.5rem] md:text-[2.25rem] font-medium">
+            {'회원가입'}
+          </h1>
+          <SignupForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
