100fd27d72fa8509f323b08efb9bf521e7e50e8c
moonjw0 <jjwon1230@naver.com>
2025-02-10 11:41:28 +0900

    💄 design: 로그인/회원가입 높이 조정

--- src/pages/signup/index.page.tsx
+++ src/pages/signup/index.page.tsx
@@ -8,7 +8,7 @@ export default function Signup() {
         title="회원가입"
         description="회원가입을 통해 FitMon의 회원이 되어보세요!"
       />
-      <div className="w-full h-[calc(100vh-81px)] flex justify-center items-center">
+      <div className="w-full min-h-[calc(100vh-81px)] flex justify-center pt-[140px]">
         <div className="flex flex-col justify-center items-center w-full max-w-[640px] md:min-w-[640px]">
           <h1 className="mb-7 md:mb-12 text-[1.5rem] md:text-[2.25rem] font-medium">
             {'회원가입'}
